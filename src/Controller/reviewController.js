0604a1c9ac2cd93c59bcd4c11f8c108bff9edf5b
iftehar <technicalkhan009@gmail.com>
2022-07-07 14:48:23 +0530

    abcd

---
+++ src/Controller/reviewController.js
@@ -0,0 +1,7 @@
+// const updatebook = async function (req,res){
+//     let bookId = req.params.bookId
+//     let reviewId = req.params.reviewId
+//     let data = req.body
+
+//     let updateReview = await ReviewModel.findOneUpdate({_id: reviewId, isDeleted : false}, {reviews: reviewId, rating, reviewedBy: revierName})
+// }
